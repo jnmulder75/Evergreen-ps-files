0ef91c759519ca7c5ad0709449303fc652a2c0c6
jnmulder75 <80901246+jnmulder75@users.noreply.github.com>
2021-03-18 15:33:01 +0100

    Create ctxwsapp.ps1

---
+++ ctxwsapp.ps1
@@ -0,0 +1,17 @@
+Write-Output 'Citrix Workspace App Start'
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)){
+    New-Item -ItemType Directory $BuildDir
+}
+$allVersions = Get-CitrixWorkspaceApp
+$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
+
+$fileName = split-path $allOnVersion.uri -Leaf -Verbose
+# $outFile = join-path 'c:\CustomizerArtifacts' $fileName
+$outFile = join-path 'c:\CustomizerArtifacts' 'CitrixWorkspaceApp.exe'
+Invoke-WebRequest $allOnVersion.uri -OutFile $outFile
+
+Start-Process -FilePath (Join-Path $BuildDir "CitrixWorkspaceApp.exe") -Argument '/silent /AutoUpdateCheck=disabled /noreboot' -Wait
+Remove-Item $outFile
+Write-Output 'Citrix Workspace App Installed'
