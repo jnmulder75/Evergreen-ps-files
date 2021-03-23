a1d8f22979454d99b5182bc725c4951d6da1f85d
jnmulder75 <80901246+jnmulder75@users.noreply.github.com>
2021-03-23 15:20:12 +0100

    Add files via upload

---
+++ DotNet.ps1
@@ -0,0 +1,20 @@
+Write-Output 'DotNET Start'
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)){
+    New-Item -ItemType Directory $BuildDir
+}
+
+$allVersions = Get-Microsoft.NET
+$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+$allOnVersion = $allVersions | Where-Object { $_.channel -eq 'Current' }
+$myVersion = $allOnVersion | Where-Object { $_.Architecture -eq 'x64'}
+$fileName = split-path $myVersion.uri -Leaf
+
+$outFile = join-path 'c:\CustomizerArtifacts' 'windowsdesktop-runtime-win-x64.exe'
+if (-not(Test-Path $outFile)) {
+    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+}
+Start-Process $outFile -Argument '/install /quiet /norestart' -Wait
+
+Remove-Item $outFile
+Write-Output 'DotNET Installed'
