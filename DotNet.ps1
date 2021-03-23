c53baa16cd8c96b005e7c58e2fa7808f61b13766
jnmulder75 <80901246+jnmulder75@users.noreply.github.com>
2021-03-23 15:20:35 +0100

    Update DotNet.ps1

--- DotNet.ps1
+++ DotNet.ps1
@@ -3,7 +3,7 @@ $BuildDir = 'c:\CustomizerArtifacts'
 if (-not(Test-Path $BuildDir)){
     New-Item -ItemType Directory $BuildDir
 }
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 $allVersions = Get-Microsoft.NET
 $mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
 $allOnVersion = $allVersions | Where-Object { $_.channel -eq 'Current' }
